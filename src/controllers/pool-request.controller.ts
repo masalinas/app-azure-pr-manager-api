2c3a7510dc07d648606e9e4c969f0922dedee30e
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2021-06-28 20:41:13 +0200

    last update

--- src/controllers/pool-request.controller.ts
+++ src/controllers/pool-request.controller.ts
@@ -48,9 +48,7 @@ export class PoolRequestController {
       },
     },
   })
-  async findById(
-    @param.path.string('username') username: string
-  ): Promise<PoolRequest> {
+  async findById( @param.path.string('username') username: string): Promise<PoolRequest> {
     return new Promise(function (resolve, reject) {
       exec('az repos pr list --organization https://dev.azure.com/ECCAIRS2/ --project ECCAIRS2 --creator ' + username + ' --status all --query "@[*].{description:description, closedDate:closedDate, reviewers:reviewers[*].{displayName:displayName}, createdBy:createdBy.uniqueName, title:title, status:status, repository:repository.name, sourceRefName:sourceRefName, targetRefName:targetRefName}" -o json', (error: any, stdout: any, stderr: any) => {
         if (error) {
